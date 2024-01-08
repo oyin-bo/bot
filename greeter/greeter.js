d41f2ff837b555cf108b438cc7f16c6eefc05c84
Oleg Mihailik <mihailik@gmail.com>
2024-01-08 21:32:11 +0000

    Terminal interface.

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -1,18 +1,36 @@
 // @ts-check
 /// <reference types="xterm" />
 
-function greeter() {
+async function greeter() {
   const termHost = document.createElement('div');
   termHost.style.cssText = `
   position: absolute;
   left: 0; top: 0;
   width: 100%; height: 100%;
+  padding: 1em;
   `;
   document.body.appendChild(termHost);
 
   /** @type {import('xterm').Terminal} */
-  const term = new Terminal({ allowTransparency: true });
-  term.open(termHost);
-  term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+  const terminal = new Terminal({
+    allowTransparency: true,
+    cursorBlink: true,
+    cursorStyle: 'underline',
+    disableStdin: false,
+    theme: {
+      background: '#00000000'
+    }
+  });
 
-} greeter;
+  const fitAddon = new (FitAddon.FitAddon || FitAddon)();
+  terminal.loadAddon(fitAddon);
+
+  terminal.open(termHost);
+  fitAddon.fit();
+  terminal.focus();
+
+  await new Promise(resolve => setTimeout(resolve, 600));
+
+  terminal.write('Greeter BOT for \x1B[1;3;31mBlueSky\x1B[0m $ ');
+
+} greeter();
